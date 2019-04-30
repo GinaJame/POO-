33f53dd65fa0e65c026e246580b2092e9e61c95d
GinaJame <42384952+GinaJame@users.noreply.github.com>
2019-04-30 10:36:12 -0500

    Cambios3

--- SceneBatalla.java
+++ SceneBatalla.java
@@ -165,12 +165,18 @@ public class SceneBatalla extends Scene{
                         String malo=p.getTipo();
                         switch(malo){
                             case "Ensalada":
-                                main.setScene4(x,y);
+                                main.setScene4(0,0);
+                            break;
+                            case "Jitomate":
+                                main.setScene3(x,y);
                             break;
                             case "HotDog":
-                                main.setScene5(x,y);
+                                main.setScene5(0,0);
                             break;
-                            default: System.out.println("no aplica");
+                            case "Trumpito":
+                                main.setScene4(x,y);
+                            break;
+                            default: main.setScene5(x,y);
                             break;
                         }
                     }   
